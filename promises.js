fa0864bb81591a79f77371b3d331447b117ff330
Mohammad Reza Salemi <en.salemi75@gmail.com>
2022-03-05 12:45:51 +0330

    use promise all and fetch api

--- promises.js
+++ promises.js
@@ -27,6 +27,19 @@ function createPost(post) {
     }, 2000);
   });
 }
-createPost({ title: "new post", body: "this is a new post" })
-  .then(getPosts)
-  .catch((err) => console.log(err));
+// createPost({ title: "new post", body: "this is a new post" })
+//   .then(getPosts)
+//   .catch((err) => console.log(err));
+
+// Promise all
+const promise1 = Promise.resolve("Hello Reza");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, "goodBye")
+);
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
+Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+  console.log(values)
+);
