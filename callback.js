d336e037a65c6d65b9e38d143f338689dddfd679
Mohammad Reza Salemi <en.salemi75@gmail.com>
2022-03-05 11:53:55 +0330

    write callback function for get and create post

---
+++ callback.js
@@ -0,0 +1,22 @@
+const posts = [
+  { title: "Post one", body: "This is post one" },
+  { title: "Post two", body: "This is post two" },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+function createPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+getPosts();
+
+createPost({ title: "Post three", body: "This is post three" });
